7511cc86056c2065bb1439ab85810c0565d192cc
Neil <Neil@192.168.2.59>
2017-11-27 21:36:43 -0500

    Merge origin/master
    Conflicts:
    	327project-backend/src/backend/BackEnd.java

--- 327project-mergetsf/src/pkg327project/mergetsf/Mergetsf.java
+++ 327project-mergetsf/src/pkg327project/mergetsf/Mergetsf.java
@@ -15,30 +15,37 @@ public class Mergetsf {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
     public static void main(String[] args) throws IOException {
 
         String line;
+        try {
+            FileWriter fw = new FileWriter(args[1]);
+            BufferedWriter bufferedWriter = new BufferedWriter(fw);
 
-        FileWriter fw = new FileWriter(mergedTransactionFile);
-        BufferedWriter bufferedWriter = new BufferedWriter(fw);
+            File dir = new File(args[0]);
+            File[] directoryListing = dir.listFiles();
 
-        File dir = new File(args[0]);
-        File[] directoryListing = dir.listFiles();
-
-        if (directoryListing != null) {
             for (File transactionFile : directoryListing) {
                 FileReader fileReader = new FileReader(transactionFile);
                 BufferedReader bufferedReader = new BufferedReader(fileReader);
 
                 while ((line = bufferedReader.readLine()) != null) {
-                    bufferedWriter.write(line);
+                    if (line.equals("EOS")) {
+                        break;
+                    } else {
+                        bufferedWriter.write(line);
+                    }
                 }
+                bufferedReader.close();
             }
-        } else {
-            // Error
+            bufferedWriter.write("EOS");
+            bufferedWriter.flush();
+            bufferedWriter.close();
+            
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
-
     }
-
 }
